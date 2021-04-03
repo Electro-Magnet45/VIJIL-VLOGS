82f772eb5cb24276011fdd9ac231cc8479615df0
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-04-03 17:51:48 +0530

    Initial Fourth Commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: "https://vijil-vlogs-server.herokuapp.com",
 });
 
 export default instance;
